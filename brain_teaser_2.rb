90f1ae7b5e604a600b14b9c9c23992366a897ca6
joshua-murphy <josh.murphy11@gmail.com>
2017-11-17 10:01:20 -0700

    add exit clause

--- brain_teaser_2.rb
+++ brain_teaser_2.rb
@@ -1,25 +1,30 @@
 require "colorize"
 
-puts "\nWhat is your word?".yellow
-@word = gets.strip.downcase
+while true
 
-@word_arr = (@word.split(%r{\s*}))
+  puts "\nWhat is your word?".yellow
+  @word = gets.strip.downcase
+  @word == "exit" ? exit() : "continue"
 
-def palindrome_check(num, fin)
-  first_position = @word_arr[num]
-  total_letters = @word_arr.length
-  last_position = @word_arr[total_letters - fin]
+  @word_arr = (@word.split(%r{\s*}))
 
-  if first_position == last_position
-    if num + 1 == total_letters
-      puts "#@word is a palindrome".yellow
+  def palindrome_check(num, fin)
+    first_position = @word_arr[num]
+    total_letters = @word_arr.length
+    last_position = @word_arr[total_letters - fin]
+
+    if first_position == last_position
+      if num + 1 == total_letters
+        puts "#@word is a palindrome".yellow
+      else
+        palindrome_check(num + 1, fin + 1)
+      end
     else
-      palindrome_check(num + 1, fin + 1)
+      puts "#@word is not a palindrome".yellow
     end
-  else
-    puts "#@word is not a palindrome".yellow
+
   end
 
-end
+  palindrome_check(0, 1)
 
-palindrome_check(0, 1)
+end
